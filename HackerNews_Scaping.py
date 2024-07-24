2555908c1e4f336a28bb75dcb1c2d2bd01fb6c60
stella <stella33299@gmail.com>
2024-07-24 17:50:19 +0800

    7/24

---
+++ HackerNews_Scaping.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+
+# Get HTML file
+response = requests.get('https://news.ycombinator.com/news')
+HTML = response.text
+
+soup = BeautifulSoup(HTML, 'html.parser')
+
+# get the first title
+article1 = soup.find(name='span', class_='titleline').find(name='a')
+article1_name = article1.get_text()    ##--> Open source AI is the path forward
+article1_link = article1.get('href')     ## --> https://about.fb.com/news/2024/07/open-source-ai-is-the-path-forward/
+article1_upvote = soup.find(name='span', class_='score').get_text() ## --> 1846 points
+
+# get all titles and info
+articleNames =[]
+articleLinks =[]
+articles = soup.find_all(name='span', class_='titleline')
+
+for item in articles:
+    article = item.find(name='a')
+    name = article.get_text()
+    articleNames.append(name)
+
+    link = article.get('href')
+    articleLinks.append(link)
+articleUpvotes = [int(score.get_text().split()[0]) for score in soup.find_all(name='span', class_='score')]    ##--turn ['1860 points',..] into [1860,..]
+
+
+# get the article that has the most upvotes
+highest_score = max(articleUpvotes)
+index = articleUpvotes.index(highest_score)
+
+a_name = articleNames[index]
+a_link = articleLinks[index]
+print(a_name, a_link, highest_score, index)
---
+++ main.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup #the latest versin of Beautiful Soup
+# import lxml  #--if soup = BeautifulSoup(html_doc, 'lxml')
+
+HTML = open('website.html', 'r')
+
+# Parsing
+soup = BeautifulSoup(HTML, 'html.parser')
+
+# Indent HTML file
+print(soup.prettify())
+
+# Tap into different parts of the web
+print(soup.title) ##return: <title>Angela's Personal Site</title>
+print(soup.title.name) ##return the tag name: title --> (for <title> tag)
+print(soup.title.string) ##return: Angela's Personal Site
+
+print(soup.a) ##return contents of the first anchor tag
+print(soup.li) ##return the first li
+print(soup.p) ##return the first paragraph
+
+
+
+# Get ALL of the xx type/contents: using func
+# Using the tag name
+all_anchor_tags = soup.find_all(name='a')
+print(all_anchor_tags) ##return a list: [<a href="https://www.appbrewery.co/">The App Brewery</a>, <a href="https://angelabauer.github.io/cv/hobbies.html">My Hobbies</a>, <a href="https://angelabauer.github.io/cv/contact-me.html">Contact Me</a>]
+
+# Get texts/values
+for tag in all_anchor_tags:
+    tag.getText() #return: The App Brewery
+
+    #get the href (hyerlink)
+    tag.get('href') #return: https://www.appbrewery.co/
+
+
+# Using the tag name + the attribute name: the only attribute
+# soup.find(tag_name, otherAttributes) ##--return the first itme that match the query
+heading1 = soup.find(name='h1', id='name')
+print(heading1) ##-- <h1 id="name">Angela Yu</h1>
+
+section_heading = soup.find(name='h3', class_='heading')
+print(section_heading) ##-- <h3 class="heading">Books and Teaching</h3>
+print(section_heading.get('class')) ##-- ['heading']
+
+
+# Use Unique element to find the content: CSS selector, or any other selector (#ID, .class,...)
+soup.select_one('CSS_selector') #--return the first matching item
+soup.select() #--return a list: of all matching items
+
+company_url = soup.select_one(selector='p, a') ##CSS selector = <p> <a> (find the 1st anchor tag sits in the paragraph tag)
+headings = soup.select('.heading') ##by .class; -- return a list
+
+HTML.close()
